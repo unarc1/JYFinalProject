e97e515058139e0b71dd6d0a0559d42edf4fcd85
Joel Andrew Bianchi <JABianchi@gmail.com>
2022-06-05 20:56:44 -0400

    cleaned up merge issues

--- src/Game.java
+++ src/Game.java
@@ -65,7 +65,7 @@ public class Game {
         Location oldLoc = new Location(userRow+1, 0);
         grid.setImage(oldLoc, null);
 
-  }
+    }
     //if I push down arrow, then plane goes down
 
 
@@ -94,134 +94,66 @@ public class Game {
   public boolean isGameOver() {
     int cRed = 0;
     int cBlack = 0;
-    for(int r = 0; r < 8; r++)
+    for(int r = 0; r < 8; r++){
       for(int c = 0; c < 8; c++){
         if(grid.getImage(new Location(r,c)) == redP) cRed++;
         if(grid.getImage(new Location(r,c)) == blackP) cBlack++;
       }
+    }
     if(cRed == 0 || cBlack == 0) return true;
     else return false;
   }
   
   public void isValidSimpleMove(){
-<<<<<<< HEAD
-=======
     
   }
   
   public void setBG(){
     grid.setImage(new Location(0,0) , redT);
-    for(int r = 0; r < 8; r++)
+    for(int r = 0; r < 8; r++){
       for(int c = 0; c < 8; c++){
         if((r+c)%2!=0) grid.setImage(new Location(r,c), blackT);
         else grid.setImage(new Location(r,c), redT);
       }
-  }
->>>>>>> origin/main
-    
-  public void setPiece(){
-    for(int r = 0; r < 8; r++)
-      for(int c = 0; c < 8; c++){
-        if(r < 3 && (r+c)%2!=0) grid.setImage(new Location(r,c), blackP);
-        else if(r > 4 && (r+c)%2!=0) grid.setImage(new Location(r,c), redP);
-      } 
-  }
-<<<<<<< HEAD
-  
-  public void setBG(){
-    grid.setImage(new Location(0,0) , redT);
-    for(int r = 0; r < 8; r++)
-      for(int c = 0; c < 8; c++){
-        if((r+c)%2!=0) grid.setImage(new Location(r,c), blackT);
-        else grid.setImage(new Location(r,c), redT);
-      }
-  }
-    
-  public void setPiece(){
-    for(int r = 0; r < 8; r++)
-      for(int c = 0; c < 8; c++){
-        if(r < 3 && (r+c)%2!=0) grid.setImage(new Location(r,c), blackP);
-        else if(r > 4 && (r+c)%2!=0) grid.setImage(new Location(r,c), redP);
-      } 
-  }
-<<<<<<< HEAD
-  
-  public void setBG(){
-    grid.setImage(new Location(0,0) , redT);
-    for(int r = 0; r < 8; r++)
-      for(int c = 0; c < 8; c++){
-        if((r+c)%2!=0) grid.setImage(new Location(r,c), blackT);
-        else grid.setImage(new Location(r,c), redT);
-      }
-  }
-    
-  public void setPiece(){
-    for(int r = 0; r < 8; r++)
-      for(int c = 0; c < 8; c++){
-        if(r < 3 && (r+c)%2!=0) grid.setImage(new Location(r,c), blackP);
-        else if(r > 4 && (r+c)%2!=0) grid.setImage(new Location(r,c), redP);
-      } 
+    }
   }
 
+  public void setPiece(){
+    for(int r = 0; r < 8; r++){
+      for(int c = 0; c < 8; c++){
+        if(r < 3 && (r+c)%2!=0) grid.setImage(new Location(r,c), blackP);
+        else if(r > 4 && (r+c)%2!=0) grid.setImage(new Location(r,c), redP);
+      } 
+    }
+  }
+  
   public void HandleMouseClick(){
     
     Location first = grid.waitForClick();
     
     while(grid.getImage(first).equals(blackP) || grid.getImage(first).equals(redP)){
     
-    if(grid.getImage(first).equals(blackP)){
-      Location second = grid.waitForClick();
-      if(grid.getImage(second).equals(blackT)){
-        grid.setImage(second, blackP);
-        grid.setImage(first, blackT);
+      if(grid.getImage(first).equals(blackP)){
+        Location second = grid.waitForClick();
+        if(grid.getImage(second).equals(blackT)){
+          grid.setImage(second, blackP);
+          grid.setImage(first, blackT);
+        }
       }
-    }
     
-    if(grid.getImage(first).equals(redP)){
-      Location second = grid.waitForClick();
-      if(grid.getImage(second).equals(blackT)){
-        grid.setImage(second, redP);
-        grid.setImage(first, blackT);
+      if(grid.getImage(first).equals(redP)){
+        Location second = grid.waitForClick();
+        if(grid.getImage(second).equals(blackT)){
+          grid.setImage(second, redP);
+          grid.setImage(first, blackT);
+        }
       }
-    }
-    first = grid.waitForClick();
+      first = grid.waitForClick();
     }
   }
   
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-}
-
-
-=======
-=======
->>>>>>> origin/main
-
   public void handleMouseClick(){
 
-    
     Location first = grid.waitForClick();
     Location second = grid.waitForClick();
 
@@ -240,39 +172,6 @@ public class Game {
         grid.setImage(first, blackT);
       }
     }
-    
-    
   }
-  
-  
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 }
-
-
-<<<<<<< HEAD
->>>>>>> origin/main
-=======
->>>>>>> origin/main
-
